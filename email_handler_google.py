0b1ec94b4e824f04a2dcc3126c9ae43a256f2d7f
Sean O'Neil <40441698+saoneil@users.noreply.github.com>
2025-07-13 19:20:23 -0300

    Added reauthentication measure when token expires. The function will delete the old token and automatically retry, prompting the user to sign in.

--- email_handler_google.py
+++ email_handler_google.py
@@ -4,6 +4,8 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
+from google.auth.exceptions import RefreshError
+
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.compose']
 
@@ -14,7 +16,12 @@ def get_gmail_service():
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+            try:
+                creds.refresh(Request())
+            except RefreshError as e:
+                print("Token refresh failed. Removing token.json and retrying...")
+                os.remove('token.json')
+                return get_gmail_service()
         else:
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
